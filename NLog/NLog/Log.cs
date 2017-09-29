6e3397756e32561f155a67deeaa0fe9cff3a5dba
王磊 <dexterw009@gmail.com>
2017-09-29 14:34:15 +0900

    add project

---
+++ NLog/NLog/Log.cs
@@ -0,0 +1,98 @@
+﻿using NLog;
+using System.Diagnostics;
+
+namespace NLogSample
+{
+    public sealed class Log
+    {
+        private static readonly Log instance = new Log();
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private Log() {}
+
+        public static Log Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// エラーログを出力します。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Error(string message)
+        {
+            var callerFrame = new StackFrame(1);
+            var methodName = callerFrame.GetMethod().Name;
+            var reflectedType = callerFrame.GetMethod().ReflectedType;
+
+            var className = string.Empty;
+            if (reflectedType != null)
+                className = reflectedType.Name;
+
+            Logger.Error(MakeMessage(className, methodName, message));
+        }
+
+        /// <summary>
+        /// 警告ログを出力します。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Warn(string message)
+        {
+            var callerFrame = new StackFrame(1);
+            var methodName = callerFrame.GetMethod().Name;
+            var reflectedType = callerFrame.GetMethod().ReflectedType;
+
+            var className = string.Empty;
+            if (reflectedType != null)
+                className = reflectedType.Name;
+
+            Logger.Warn(MakeMessage(className, methodName, message));
+        }
+
+        /// <summary>
+        /// インフォメーションログを出力します。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Info(string message)
+        {
+            var callerFrame = new StackFrame(1);
+            var methodName = callerFrame.GetMethod().Name;
+            var reflectedType = callerFrame.GetMethod().ReflectedType;
+
+            var className = string.Empty;
+            if (reflectedType != null)
+                className = reflectedType.Name;
+
+            Logger.Info(MakeMessage(className, methodName, message));
+        }
+
+        /// <summary>
+        /// デバッグログを出力します。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        public void Debug(string message)
+        {
+            var callerFrame = new StackFrame(1);
+            var methodName = callerFrame.GetMethod().Name;
+            var reflectedType = callerFrame.GetMethod().ReflectedType;
+
+            var className = string.Empty;
+            if (reflectedType != null)
+                className = reflectedType.Name;
+
+            Logger.Debug(MakeMessage(className, methodName, message));
+        }
+
+        /// <summary>
+        /// 出力文を作成します。
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        private string MakeMessage(string className, string methodName, string message)
+        {
+            return $"{className}, {methodName}, {message}";
+        }
+    }
+}
---
+++ NLog/NLog/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NLogSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Instance.Debug("Debug A");
+
+            Log.Instance.Info("Info B");
+
+            Log.Instance.Warn("Warning C");
+
+            Log.Instance.Error("Error D");
+        }
+    }
+}
